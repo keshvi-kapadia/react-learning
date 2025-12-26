a53abd5b171e5ca7daa11a63943115ad52362c94
keshvi-kapadia <keshvi05072005@email.com>
2025-12-26 19:16:32 +0530

    Removed unnecessary states and Disabled add Button when no input

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,19 @@ import Employee from "./Emplyee.jsx";
 import { useState } from "react";
 
 function App() {
-  const [Employees,setEmployees]=useState(["Keshvi","Khushi"]);
+  const [Employees, setEmployees] = useState(["Keshvi", "Khushi"]);
   // let Employees=["Keshvi","Khushi","Shruti","Harshita","Jiya"];
-  const [showList,setshowList]=useState(false);
-  const [empVal,setempVal]=useState("");
-  const [noEmp,setNoEmp]=useState(false);
+  const [showList, setshowList] = useState(false);
+  const [empVal, setempVal] = useState("");
 
-  function addEmp()
-  {
-    setEmployees(()=>[...Employees,empVal]);
+  function addEmp() {
+    setEmployees(() => [...Employees, empVal]);
     setempVal("");
-    setNoEmp(false);
   }
   return (
     <>
       <Intro />
-{/*       
+      {/*       
       <div className="employeeList">
         <h2>Employee List - Props</h2>
         <Employee name="Keshvi" />
@@ -30,42 +27,42 @@ function App() {
 
       <div className="employeeList">
         <h2>Add Employee</h2>
-        <input type="text" value={empVal} placeholder="Enter Name" id="addEmpib" onChange={(e)=>setempVal(e.target.value)}/>
-        <button id="addBtn" onClick={addEmp}>Add</button>
+        <input
+          type="text"
+          value={empVal}
+          placeholder="Enter Name"
+          id="addEmpib"
+          onChange={(e) => setempVal(e.target.value)}
+        />
+        <button id="addBtn" onClick={addEmp} disabled={empVal === ""}>
+          Add
+        </button>
         <h2>Employee List - Using Array Map Function</h2>
-        <button id="showHideButton" onClick={(e)=>
-          {
+        <button
+          id="showHideButton"
+          onClick={(e) => {
             setshowList(!showList);
-          }
-        }>{showList ? "Hide List" : "Show List"}</button>
+          }}
+        >
+          {showList ? "Hide List" : "Show List"}
+        </button>
 
-        {
-          showList && !noEmp &&
-          Employees.map((emp,index)=><Employee key={index} name={emp} 
-          onEmployeeClick={
-            () => 
-              {
+        {showList &&
+          Employees.length > 0 &&
+          Employees.map((emp, index) => (
+            <Employee
+              key={index}
+              name={emp}
+              onEmployeeClick={() => {
                 console.log(emp);
-              }
-          } 
-          
-          onDeleteEmployee={()=>
-          {
-            setEmployees(()=>Employees.filter((e)=>e!==emp));
-            if(Employees.length==1)
-            {
-              setNoEmp(true);
-            }
-          }
-          }
-          />)
-        }
+              }}
+              onDeleteEmployee={() => {
+                setEmployees(() => Employees.filter((e) => e !== emp));
+              }}
+            />
+          ))}
 
-        {
-          noEmp && showList &&
-          <h2>No Employee to list</h2>
-        }
-        
+        {Employees.length == 0 && showList && <h2>No Employee to list</h2>}
       </div>
 
       <Footer />
